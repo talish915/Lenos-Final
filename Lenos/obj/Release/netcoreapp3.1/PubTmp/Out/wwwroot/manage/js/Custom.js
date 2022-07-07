f600e1bb7e917fd504bda30a6a2945d847fbf82a
talish915 <amilfn@code.edu.az>
2022-07-07 11:58:47 +0400

    Lenos Final

---
+++ Lenos/obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/manage/js/Custom.js
@@ -0,0 +1,87 @@
+﻿$(document).ready(function () {
+    $(document).on("click", "#deleteCategory", function (e) {
+        e.preventDefault();
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                let url = $(this).attr("href");
+                fetch(url).then(response => {
+                    if (response.ok) {
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        )
+                    }
+
+                    return response.text();
+                }).then(data => {
+                    $(".dataTable").html(data);
+                })
+            }
+        })
+
+    })
+
+    $(document).on("click", "#restoreCategory", function (e) {
+        e.preventDefault();
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, Restore it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                let url = $(this).attr("href");
+                fetch(url).then(response => {
+                    if (response.ok) {
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        )
+                    }
+
+                    return response.text();
+                }).then(data => {
+                    $(".dataTable").html(data);
+                })
+            }
+        })
+
+    })
+
+    $(document).on("click", "#deleteImage", function (e) {
+        e.preventDefault();
+
+        let url = $(this).attr("href");
+
+        fetch(url).then(response => { return response.text() }).then(data => { $(".productupdate").html(data) });
+    })
+
+    var loadFileMain = function (event) {
+        var output = document.getElementById('MainOutput');
+        output.src = URL.createObjectURL(event.target.files[0]);
+        output.onload = function () {
+            URL.revokeObjectURL(output.src)
+        }
+    };
+    var loadFileHover = function (event) {
+        var output = document.getElementById('HoverOutput');
+        output.src = URL.createObjectURL(event.target.files[0]);
+        output.onload = function () {
+            URL.revokeObjectURL(output.src)
+        }
+    };
+
+})
